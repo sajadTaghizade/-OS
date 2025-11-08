b2d80adb597744701744743867272cd1ddb60bc2
deniz-mn <deniz.mostafazadegan@gmail.com>
2025-11-08 17:49:22 +0330

    Add test

---
+++ lab2/codes/test_simplearith.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int a = 5, b = 3;
+  int r = simple_arith(a, b);
+  
+  printf(1, "user: simple_arith(%d,%d) returned %d\n", a, b, r);
+  exit();
+}
--- lab2/codes/user.h
+++ lab2/codes/user.h
@@ -24,6 +24,8 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 int make_duplicate(const char*);
+int simple_arith(int a, int b);
+
 
 // ulib.c
 int stat(const char*, struct stat*);
